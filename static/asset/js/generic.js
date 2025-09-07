594e0cefe60306f196f1f97dc06eedfe34a06302
LMare <marescaux.louis@gmail.com>
2025-09-07 22:49:00 +0200

    Ajout style avec TailWindCss/DaisyUI

---
+++ static/asset/js/generic.js
@@ -0,0 +1,30 @@
+
+
+/**
+ * Add RGAA attributes on the lines of the tables
+ */
+function enrichRgaaTableRows(tbody, columnIds = []) {
+	const rows = tbody.querySelectorAll("tr");
+	let prefixId = "row";
+	rows.forEach((row, rowIndex) => {
+		const th = row.querySelector("th");
+		let rowId = "";
+		if (th) {
+			if(tbody.id){
+				prefixId = `${prefixId}-row`;
+			} else {
+				console.warn("No id on the tbody", tbody);
+			}
+			rowId = `${prefixId}-${rowIndex}`;
+			th.id = rowId;
+		}
+
+		const tds = row.querySelectorAll("td");
+		tds.forEach((td, i) => {
+			const colId = columnIds[i];
+			if (colId) {
+				td.setAttribute("headers", `${rowId} ${colId}`);
+			}
+		});
+	});
+}
