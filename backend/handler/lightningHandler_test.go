0b5f7142e5770d3b1af28fd75b053087fc0fcee4
Louis <33813639+LMare@users.noreply.github.com>
2026-03-08 23:19:06 +0100

    remove / comment previous tests

--- backend/handler/lightningHandler_test.go
+++ backend/handler/lightningHandler_test.go
@@ -1,16 +1,12 @@
 package handler
 
 import (
-	"context"
-	"net/http"
-	"net/http/httptest"
-	"strings"
 	"testing"
 
-	lnrpc "github.com/Lmare/lightning-playground/backend/gRPC/github.com/lightningnetwork/lnd/lnrpc"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 )
+
+/*
 
 // MockLightningClient mocks the gRPC Lightning client
 type MockLightningClient struct {
@@ -33,54 +29,53 @@ func (m *MockLightningClient) ConnectPeer(ctx context.Context, req *lnrpc.Connec
 	return args.Get(0).(*lnrpc.ConnectPeerResponse), args.Error(1)
 }
 
+*/
+
 func TestTruncateUri(t *testing.T) {
 	tests := []struct {
-        name     string
-        uri		 string
+		name     string
+		uri      string
 		len      int
-        expected string
-    }{
-        {"not uri lnd", "azertyuiop", 3, "azertyuiop"},
-        {"uri lnd trunc 3", "azertyuiop@1.2.3.4:1234", 3, "aze...iop@1.2.3.4:1234"},
-        {"uri lnd trunc 1", "azertyuiop@1.2.3.4:1234", 1, "a...p@1.2.3.4:1234"},
+		expected string
+	}{
+		{"not uri lnd", "azertyuiop", 3, "azertyuiop"},
+		{"uri lnd trunc 3", "azertyuiop@1.2.3.4:1234", 3, "aze...iop@1.2.3.4:1234"},
+		{"uri lnd trunc 1", "azertyuiop@1.2.3.4:1234", 1, "a...p@1.2.3.4:1234"},
 		{"uri lnd not trunc", "azertyuiop@1.2.3.4:1234", 10, "azertyuiop@1.2.3.4:1234"},
-    }
+	}
 
-    for _, tt := range tests {
-        t.Run(tt.name, func(t *testing.T) {
-            result := truncateUri(tt.uri, tt.len)
-            if result != tt.expected {
-                t.Errorf("got %s, want %s", result, tt.expected)
-            }
-        })
-    }
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := truncateUri(tt.uri, tt.len)
+			if result != tt.expected {
+				t.Errorf("got %s, want %s", result, tt.expected)
+			}
+		})
+	}
 }
-
-
-
 
 func TestTruncate(t *testing.T) {
 	tests := []struct {
-        name     string
-        uri		 string
+		name     string
+		uri      string
 		len      int
-        expected string
-    }{
-        {"string trunc 3", "azertyuiop", 3, "aze...iop"},
+		expected string
+	}{
+		{"string trunc 3", "azertyuiop", 3, "aze...iop"},
 		{"uri lnd trunc 3", "azertyuiop@1.2.3.4:1234", 3, "aze...234"},
-        {"string trunc 1", "azertyuiop", 1, "a...p"},
+		{"string trunc 1", "azertyuiop", 1, "a...p"},
 		{"string not trunc", "azertyuiop", 5, "azertyuiop"},
 		{"string not trunc 20", "azertyuiop", 20, "azertyuiop"},
-    }
+	}
 
-    for _, tt := range tests {
-        t.Run(tt.name, func(t *testing.T) {
-            result := truncate(tt.uri, tt.len)
-            if result != tt.expected {
-                t.Errorf("got %s, want %s", result, tt.expected)
-            }
-        })
-    }
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := truncate(tt.uri, tt.len)
+			if result != tt.expected {
+				t.Errorf("got %s, want %s", result, tt.expected)
+			}
+		})
+	}
 }
 
 // Test isValidFormatOfId
@@ -107,6 +102,9 @@ func TestIsValidFormatOfId(t *testing.T) {
 		})
 	}
 }
+
+/*
+
 
 // Test handleShowUri with invalid ID
 func TestHandleShowUri_InvalidID(t *testing.T) {
@@ -317,3 +315,5 @@ func TestHandleUpdateNodeAlias_MissingID(t *testing.T) {
 	// Check the status code - should be 500 (fail)
 	assert.Equal(t, http.StatusInternalServerError, rr.Code)
 }
+
+*/
